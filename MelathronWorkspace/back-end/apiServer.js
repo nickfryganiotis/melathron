0a88fe04f6696899f5bce20645c0f937d1e03b19
nickfryganiotis <nikolasfryganiotis@yahoo.com>
2021-04-15 01:00:09 +0300

    add_apotelesma

--- MelathronWorkspace/back-end/apiServer.js
+++ MelathronWorkspace/back-end/apiServer.js
@@ -728,3 +728,14 @@ app.post( '/add_salesman', ( req, res ) => {
     } );
 
 } );
+
+app.post( '/add_apotelesma', ( req, res ) => {
+
+    const apotelesma = req.body;
+    const query = "UPDATE customer SET apotelesma_id = (SELECT apotelesma_id FROM apotelesma WHERE apotelesma_name = ? AND subapotelesma_name = ? AND continent_id = ? WHERE spcode = ?"
+    connection.query( query, [ apotelesma[ 'apotelesma_name' ], apotelesma[ 'subapotelesma_name' ], apotelesma[ 'continent_id' ], apotelesma[ 'spcode' ] ], function( error, results ) {
+        if ( error ) throw error;
+        res.sendStatus(200);
+    });
+
+} );
